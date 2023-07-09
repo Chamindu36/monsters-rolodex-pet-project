7cbd3a8e411b735c3df1d8698859f2008847b7a3
Chamindu <cudakara@gmail.com>
2023-07-09 23:04:14 +0530

    Migrate to functional components from class comps

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,37 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
+// class CardList extends Component {
 
-  render() {
-    const { monsters } = this.props;
+//   render() {
+//     const { monsters } = this.props;
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return (
-            <Card monster={monster} />
-          )
-        })}
-      </div>
-    );
-  }
+//     return (
+//       <div className="card-list" key="list">
+//         {monsters.map((monster) => {
+//           return (
+//             <Card monster={monster} />
+//           )
+//         })}
+//       </div>
+//     );
+//   }
+// };
+
+// using functional components
+const CardList = ({ monsters }) => {
+
+  return (
+    <div className="card-list" key="list">
+      {monsters.map((monster) => {
+        return (
+          <Card monster={monster} />
+        )
+      })}
+    </div>
+  );
 };
 
 export default CardList;
